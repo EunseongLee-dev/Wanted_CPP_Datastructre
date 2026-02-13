b327db11914176caead65c34825e1ce78d0948bb
EunseongLee-dev <giver1902@gmail.com>
2026-02-13 16:01:36 +0900

    이중 연결 리스트 구현
    - 첫 위치/마지막 위치에 추가하는 함수만 구현 (일반 Insert 함수 없음 -> 구현 시도 해보기)
    - 삭제/검색/출력 함수 구현

---
+++ 4.DoublyLinkedList/Main.cpp
@@ -0,0 +1,25 @@
+#include "LinkedList/LinkedList.h"
+
+#include <iostream>
+
+int main()
+{
+	LinkedList<int> list;
+	for (int ix = 0; ix < 10; ++ix)
+	{
+		list.InsertLast((ix + 1) * 10);
+	}
+
+	std::cout << "리스트 추가후 출력\n";
+	list.Print();
+
+	// 항목 제거
+	list.Delete(20);
+	list.Delete(50);
+	list.Delete(80);
+
+	std::cout << "리스트 제거 후 출력\n";
+	list.Print();
+
+	std::cin.get();
+}
