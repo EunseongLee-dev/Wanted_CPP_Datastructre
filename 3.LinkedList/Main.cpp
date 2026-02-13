a5cdcccb06743f0263fd598777a6aa9c1aed18ee
EunseongLee-dev <giver1902@gmail.com>
2026-02-13 12:03:12 +0900

    LinkedList Insert/Delete/Print 함수 구현 (현재 메모리 릭 있음)
    - Insert에서 노드 삽입
    - Delete에서 노드 제거
    - Print에서 노드 순회하면서 값 출력
    - 소멸자 구현 해야함 (메모리 정리)

--- 3.LinkedList/Main.cpp
+++ 3.LinkedList/Main.cpp
@@ -1,10 +1,26 @@
-#include "LinkedList/Node.h"
+#include "LinkedList/LinkedList.h"
 
 #include <iostream>
 
 int main()
 {
-	Node<int> node;
+	LinkedList<int> list;
+	list.Insert(20);
+	list.Insert(30);
+	list.Insert(10);
+
+	// 출력
+	std::cout << "노드 추가 후 출력\n";
+	list.print();
+
+	// 삭제
+	list.Delete(30);
+	list.Delete(10);
+	
+	// 출력
+	std::cout << "삭제 후 출력\n";
+	list.print();
+
 
 	std::cin.get();
 }
